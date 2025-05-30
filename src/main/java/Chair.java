ffbe5bff731d046cc4c3405bfd7485d2003c8ecc
marcus-wong-ics4u1-02-25 <marcus.wong5@student.tdsb.on.ca>
2025-05-30 15:15:56 +0000

    same assignment 4.3

---
+++ src/main/java/Chair.java
@@ -0,0 +1,29 @@
+public class Chair {
+    
+    private int legs;
+    private String material;
+
+    public Chair() {
+        this.legs = 4;
+        this.material = "wood";
+    }
+
+    public Chair(int legs) {
+        this.legs = legs;
+        this.material = "wood";
+    }
+
+    public Chair(int legs, String material) {
+        this.legs = legs;
+        this.material = material;
+    }
+
+    public int getLegs() {
+        return this.legs;
+    }
+
+    public String getMaterial() {
+        return this.material;
+    }
+}
+
---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,28 @@
+public class Rectangle {
+
+    private double length;
+    private double width;
+
+    public Rectangle() {
+        this.length = 4.0;
+        this.width = 8.0;
+    }
+
+    public Rectangle(double length) {
+        this.length = length;
+        this.width = 8.0;
+    }
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return this.length;
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+}
